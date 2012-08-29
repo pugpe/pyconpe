b3e4c83b0673c31effe4f84786e9aef381533c80
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-08-29 15:26:22 -0300

    Ajustes de deploy e adição de 404 e 500
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- pyconpe/urls.py
+++ pyconpe/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'pyconpe.views.home', name='home'),
+   url(r'^$', 
+        'django.views.generic.simple.redirect_to',
+        {'url': 'http://pug-pe.python.org.br/encontro/xix/'},
+    ),
     url(r'^submissao/', include('submission.urls', namespace='submission')),
 
     url(r'^admin/', include(admin.site.urls)),
