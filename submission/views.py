fa24c1fc7982b3633e07abf0718b4adb3c32a2b7
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-08-29 09:48:49 -0300

    Corrigido form_valid() e adicionado admin
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- submission/views.py
+++ submission/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.utils.functional import lazy 
 
 from .models import Talk
 from .forms import TalkForm
@@ -13,7 +14,9 @@ class SubmissionView(CreateView):
     form_class = TalkForm
     template_name = 'submission/submission.html'
     model = Talk
+    success_url = lazy(reverse, str)('submission:submission')
 
     def form_valid(self, form):
+        response = super(SubmissionView, self).form_valid(form)
         messages.success(self.request, _(u'Palestra submetida com sucesso!'))
-        return HttpResponseRedirect(reverse('submission:submission'))
+        return response
