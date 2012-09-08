2625a7efe9bc1cc2cc9b930dee20edc3f5d21b9f
Fernando Rocha <fernandogrd@yahoo.com.br>
2012-09-08 19:40:14 -0300

    View de optout
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- emails/views.py
+++ emails/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+# -*- coding:utf-8 -*-
+from django.shortcuts import render
+
+from submission.utils import token_required
+
+
+@token_required
+def optout(request):
+    email = request.session['email']
+    email.opt_in = False
+    email.save()
+    return render(request, 'emails/optout.html', {})
--- pyconpe/urls.py
+++ pyconpe/urls.py
@@ -4,11 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-   url(r'^$', 
-        'django.views.generic.simple.redirect_to',
-        {'url': '/submissao/', 'permanent': False},
+   url(r'^$',
+       'django.views.generic.simple.redirect_to',
+       {'url': '/submissao/', 'permanent': False},
     ),
     url(r'^submissao/', include('submission.urls', namespace='submission')),
+    url(r'^emails/', include('emails.urls', namespace='emails')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
