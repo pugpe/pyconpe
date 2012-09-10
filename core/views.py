1c76e9f431afbb3667a61c8d41cd7b2ecdf81e53
Renato Oliveira <renatooliveira.cin@gmail.com>
2012-09-09 22:46:33 -0300

    Corrigindo pequeno erro

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def results(request):
         talk = {}
         talk['likes'] =  t.vote_set.filter(type='like').count()
         talk['dislikes'] =  t.vote_set.filter(type='dislike').count()
-        talk['name'] = t
+        talk['name'] = t.name
         talk_list.append(talk)
     ctx['talks'] = sorted(talk_list, key=lambda x:x['likes'], reverse=True)
     return render(request, 'core/results.html', ctx)
